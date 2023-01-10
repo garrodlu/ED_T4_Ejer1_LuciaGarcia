642ff39c049e351c87ff4b33a5e9cd8eb496a682
lucia <garrodlu@lab03-09.gregoriofer.local>
2023-01-10 13:23:57 +0100

    Primera confirmación

---
+++ ED_T4_Ejer1/src/ed_t4_ejer1/ED_T4_Ejer1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ed_t4_ejer1;
+
+/**
+ *
+ * @author garrodlu
+ */
+public class ED_T4_Ejer1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
